3ad9f9a634e721a8aaeb1554e2303d3116c7c892
tyaa <tyaa@ukr.net>
2019-05-11 11:23:21 +0300

    init

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author student
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // int x = 100500;
+        // long y = 4543658768979089L;
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input x (long): ");
+        long x = sc.nextLong();
+        
+        System.out.println(x);
+    }
+    
+}
